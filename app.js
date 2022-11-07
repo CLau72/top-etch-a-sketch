ad5cf17fe947e5110e2298fc0d492c738a026faf
Carson Lau <calau@ra.rockwell.com>
2022-11-07 11:17:18 -0500

    Build basic Etch-a-sketch functionality

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const CANVASSIZE = 960;
+const CANVASSIZE = 720;
 const canvas = document.getElementById("canvas")
 
 function createCanvas(pixelSize)  {
   console.log("Creating Canvas")
   const pixelsPerPixel = Math.sqrt(Math.pow(CANVASSIZE,2) / Math.pow(pixelSize,2))
-  for (let i = 0; i <= Math.pow(pixelSize,2); i++) {
+  for (let i = 0; i < Math.pow(pixelSize,2); i++) {
     const pixel = document.createElement("div");
+    pixel.classList.add('pixel')
     pixel.style.width = `${pixelsPerPixel}px`;
     pixel.style.height = `${pixelsPerPixel}px`;
-    pixel.style.background = "blue";
-    pixel.innerText = `${i}`
      canvas.appendChild(pixel);
   }
-
-  
 };
 
-createCanvas(16);
+createCanvas(100);
+
+const pixel = document.querySelectorAll('.pixel');
+
+pixel.forEach((pixel) => {
+  pixel.addEventListener('mouseover', () => {
+    pixel.classList.add("dark");
+  });
+});
+
